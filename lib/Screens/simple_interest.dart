0f58f0b894d9a754efd6861f9600e4a25486ffe5
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-14 21:45:35 +0545

    interest

--- lib/Screens/simple_interest.dart
+++ lib/Screens/simple_interest.dart
@@ -16,8 +16,63 @@ class _SimpleInterestState extends State<SimpleInterest> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: Text("Simple Interest"),
+        backgroundColor: Colors.amber,
       ),
+      body: Padding(padding: EdgeInsetsGeometry.all(20),
+      child: Column(
+        children: [
+
+          SizedBox(height: 20,),
+
+          TextField(
+            onChanged: (value) {
+              principal =  double.parse(value);
+            },
+            decoration: InputDecoration(
+              labelText: "Enter Principal",
+              hintText: "eg. 2000",
+              border: OutlineInputBorder()
+            ),
+          ),
+
+          SizedBox(height: 20,),
+
+          TextField(
+            onChanged: (value) {
+              rate = double.parse(value);
+            },
+            decoration: InputDecoration(
+              labelText: "Enter Rate",
+              border: OutlineInputBorder(),
+            ),
+          ),
+
+          SizedBox(height: 20,),
+
+          TextField(
+            onChanged: (value){
+              time = double.parse(value);
+            },
+            decoration: InputDecoration(
+              labelText: "Enter Time",
+              border: OutlineInputBorder()
+            ),
+          ),
+
+          SizedBox(height: 20,),
+
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(onPressed: (){
+              interest = (principal*rate*time)/100;
+            }, child: Text("Calculate Interest")),
+          ),
+          Text("Simple Interest: ${interest}"),
+        ],
+      ),
+      ),
+
     );
     }
 }
