ba3498c9828825b6f6f9a6d0efcf03867bd2325d
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-15 20:53:03 +0545

    palindrome

--- lib/Screens/palindrome.dart
+++ lib/Screens/palindrome.dart
@@ -8,8 +8,45 @@ class Palindrome extends StatefulWidget {
 }
 
 class _PalindromeState extends State<Palindrome> {
+  int number = 0;
+  String result = "";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Palindrome")
+      ),
+      body: 
+      Padding(
+        padding: EdgeInsetsGeometry.all(20),
+          child: Column(
+            children: [
+
+              TextField(
+                onChanged: (value) {
+                  number = int.parse(value);
+                },
+                decoration: InputDecoration(
+                  labelText: "Enter a Number",
+                  hintText: "eg. 151",
+                  border: OutlineInputBorder(),
+                ),
+                
+              ),
+              SizedBox(height: 30,),
+
+              SizedBox(
+                child: ElevatedButton(onPressed: (){
+                  setState(() {
+                    
+                  });
+                }, child: Text("Check if the Number is Palindrome")),
+              ),
+
+            ],
+
+        ),),
+    );
   }
 }
