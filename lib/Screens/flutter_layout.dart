4551febf32a39713cc26231b37de8c8a9b0e7585
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-19 10:33:55 +0545

    flutterLayout

--- lib/Screens/flutter_layout.dart
+++ lib/Screens/flutter_layout.dart
@@ -9,7 +9,27 @@ class FlutterLayoutScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter Layout Screen"),
         backgroundColor: Colors.amber,
-        
+      ),
+      body: Container(
+        width: 300,
+        height: 200,
+        padding: EdgeInsets.only(left: 20,top:50),
+        decoration:  BoxDecoration(
+         border: Border.all(color: Colors.black)
+        ),
+       
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+
+            Column(children: [Icon(Icons.call),Text("Call")],),
+            Spacer(),
+            Column(children: [Icon(Icons.route),Text("Route")],),
+            Spacer(),
+            
+
+          ],
+        ),
       ),
     );
   }
