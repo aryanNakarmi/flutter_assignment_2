e9cfd8bc0a4215a9f4be9681fc09aa30a9a9f031
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-23 12:41:53 +0545

    snackBar

--- lib/Screens/flexible_expanded_screen.dart
+++ lib/Screens/flexible_expanded_screen.dart
@@ -16,12 +16,18 @@ class FlexibleExpandedScreen extends StatelessWidget {
           Expanded(
             flex: 1,
             child: GestureDetector(
-              onTap: ()=> print("This is container1"),
+              onTap: ()=> ScaffoldMessenger.of(context).showSnackBar(
+                const  SnackBar(content: Text("Container 1"),
+                backgroundColor: Colors.green,
+                duration: Duration(seconds: 3),
+                behavior: SnackBarBehavior.floating,)
+              ),
               child: Container(
                 alignment: Alignment.center,
                 width: double.infinity,
                 color: Colors.green,
-                child: Text("1",style: TextStyle(fontSize: 25),),
+                child: Text("1",style: TextStyle(fontSize: 25),
+                ),
               ),
             ),
           ),
@@ -29,7 +35,12 @@ class FlexibleExpandedScreen extends StatelessWidget {
             flex: 2,
             child: GestureDetector(
 
-              onTap: ()=> print("This is container2"),
+              onTap: ()=> ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Container 2",style: TextStyle(fontSize: 20),),
+                duration: Duration(seconds: 2),
+                behavior: SnackBarBehavior.floating,
+                backgroundColor: Colors.blue,)
+              ),
               child: Container(
                 alignment: Alignment.center,
                 width: double.infinity,
