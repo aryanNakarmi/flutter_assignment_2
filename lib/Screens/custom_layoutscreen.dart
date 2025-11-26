ac2d1c3509a5c8deea30d3883fda28c07f250f3a
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-26 09:22:51 +0545

    widget

--- lib/Screens/custom_layoutscreen.dart
+++ lib/Screens/custom_layoutscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+class CustomLayoutscreen extends StatelessWidget {
+  const CustomLayoutscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
