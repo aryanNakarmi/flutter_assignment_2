8522b302125bdb3e2fcb10ee5993fcce0c46c25b
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-26 09:51:40 +0545

    custom

--- lib/Screens/custom_layoutscreen.dart
+++ lib/Screens/custom_layoutscreen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_assignment_2/app.dart';
 
 class CustomLayoutscreen extends StatelessWidget {
   const CustomLayoutscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Custom Buttom Screen"),
+      )
+    );
   }
 }
